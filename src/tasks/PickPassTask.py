68df22cbd3d430e8be00b4ba5a7a5c73df1e4f44
HoshimiRIN <admin@sakuramoe.dev>
2025-11-01 01:35:30 +0800

    feat(PickPassTask): add 60s check duration to reduce resource usage

--- src/tasks/PickPassTask.py
+++ src/tasks/PickPassTask.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 from src.tasks.SRTriggerTask import SRTriggerTask
 
@@ -9,8 +10,12 @@ class PickPassTask(SRTriggerTask):
         self.name = "自动领取月卡"
         self.description = "弹出月卡界面后自动领取月卡"
         self.trigger_count = 0
+        self.last_check_time = time.time()
 
     def run(self):
-        if box:=self.ocr(0.44, 0.94, 0.56, 1, match='点击空白处关闭'):
-            self.click_box(box)
+        now = time.time()
+        if now - self.last_check_time > 60:
+            self.last_check_time = time.time()
+            if box:=self.ocr(0.44, 0.94, 0.56, 1, match='点击空白处关闭'):
+                self.click_box(box)
         return
