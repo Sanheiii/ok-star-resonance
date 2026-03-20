b07fe3b8c4cd75578815d50572bd9b50eb266621
三黑 <w953934508@outlook.com>
2026-03-20 22:58:35 +0800

    修复可能的崩溃

--- src/tasks/FishingTask.py
+++ src/tasks/FishingTask.py
@@ -20,8 +20,8 @@ class FishingTask(SRTriggerTask):
             'Disable Yolo Detect': False,
             'Switch Pole Key': 'm',
             'Fishing Interruption Notice': False,
-            'PushDeer Server': og.app.tr('Put your server url here if not using official server'),
-            'PushDeer ApiKey': og.app.tr('Get from your PushDeer app')
+            'PushDeer Server': og.app.tr('Put your server url here if not using official server') if og.app else None,
+            'PushDeer ApiKey': og.app.tr('Get from your PushDeer app') if og.app else None,
         })
         
         self.trigger_count = 0
