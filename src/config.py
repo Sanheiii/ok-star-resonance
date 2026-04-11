20dd1953abe5a740a8ed4d9a8169bf2233162401
三黑 <w953934508@outlook.com>
2026-04-11 23:30:51 +0800

    Update README.md

--- src/config.py
+++ src/config.py
@@ -88,7 +88,6 @@ config = {
         ["src.tasks.AimPurchaseButtonTask", "AimPurchaseButtonTask"],
         ["src.tasks.AutoSkillTask", "AutoSkillTask"],
         ["src.tasks.AutoPartyTask", "AutoPartyTask"],
-        ["src.tasks.MissionEntryTask", "MissionEntryTask"],
-        ["src.tasks.AutoReviveTask", "AutoReviveTask"]
+        ["src.tasks.MissionEntryTask", "MissionEntryTask"]
     ]
 }
