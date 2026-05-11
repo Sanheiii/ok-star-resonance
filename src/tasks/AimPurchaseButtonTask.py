3761d82e67827a93ca41cf52441313db373790a7
Sanheiii <35133371+Sanheiii@users.noreply.github.com>
2026-05-11 10:30:30 +0800

    更新麻将“和”的模板，在移动到交易行购买按钮后1秒内找到确认按钮则移动到确认按钮上

--- src/tasks/AimPurchaseButtonTask.py
+++ src/tasks/AimPurchaseButtonTask.py
@@ -1,5 +1,3 @@
-import re
-
 from src.tasks.SRTriggerTask import SRTriggerTask
 
 class AimPurchaseButtonTask(SRTriggerTask):
@@ -12,10 +10,18 @@ class AimPurchaseButtonTask(SRTriggerTask):
 
     def run(self):
         if box:=self._find_purchase():
-            target = box.center()
-            self.move(target[0], target[1])
+            self._move_to_box(box)
+            self._move_to_confirm()
             self.disable()
         return
+
+    def _move_to_confirm(self):
+        if box:=self.wait_until(self._find_confirm, time_out=1):
+            self._move_to_box(box)
+
+    def _move_to_box(self, box):
+        target = box.center()
+        self.move(target[0], target[1])
 
     def _find_purchase(self):
         language = self.get_game_language()
@@ -25,3 +31,14 @@ class AimPurchaseButtonTask(SRTriggerTask):
             return self.find_one("purchase_jp", box=self.box_of_screen(0.90, 0.90, 0.985, 0.95))
         else:
             return self.find_one("purchase_en", box=self.box_of_screen(0.90, 0.90, 0.985, 0.95))
+
+    def _find_confirm(self):
+        language = self.get_game_language()
+        if language == 'zhs':
+            return self.find_one("confirm")
+        elif language == 'zht':
+            return self.find_one("confirm_zht", box=self.box_of_screen(0.83, 0.89, 0.98, 0.94))
+        elif language == 'jp':
+            return self.find_one("confirm_jp", box=self.box_of_screen(0.83, 0.89, 0.98, 0.94))
+        else:
+            return self.find_one("confirm_en", box=self.box_of_screen(0.83, 0.89, 0.98, 0.94))
